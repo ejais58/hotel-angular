73b71e34a64949f16b1bea94d4df17d06cc037de
Eliasjais <73757035+ejais58@users.noreply.github.com>
2024-01-12 09:27:43 -0300

    feat/conectionApi
    se realizo la conexion de la api para el login

--- src/app/feature/login/page/login-page/login-page.component.ts
+++ src/app/feature/login/page/login-page/login-page.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
@@ -7,14 +10,44 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required]);
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
+  constructor(private httpClient: HttpClient, private fb: FormBuilder, private router: Router){}
+
+  base_url: string = 'http://localhost:3000';
+
+  miFormulario: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  })
+
+  getErrorMessage(controlName: string): string {
+    const control = this.miFormulario.get(controlName);
+
+    if (control?.hasError('required')) {
       return 'You must enter a value';
     }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    return control?.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  login() {
+    
+    const email = this.miFormulario.get('email')?.value;
+    const password = this.miFormulario.get('password')?.value;
+    
+    return this.httpClient.post(`${this.base_url}/auth/login`, {email,password}).subscribe((respuesta: any) =>{
+      console.log(respuesta);
+
+      if (respuesta.token) {
+        // Guardar el token en localStorage
+        localStorage.setItem('token', respuesta.token);
+  
+        // Redireccionar después de una respuesta exitosa
+        this.router.navigate(['/home']);
+      } else {
+        console.error(respuesta);
+      }
+      
+    })
   }
 
   hide = true;
